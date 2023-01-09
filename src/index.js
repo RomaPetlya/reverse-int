27ca3839eeb761c679652caae42fe3cec7bfb5db
RomaPetlya <jeremy_wade@mail.ru>
2023-01-09 10:43:57 +0300

    feat : reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const array = Array.from(String(n));
+    let str = String(array.reverse());
+    str = str.split(',').join('');
+    return str.replace(/[^0-9]/g, '');
 }
